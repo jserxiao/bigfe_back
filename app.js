c3de533516f99ac31b70583d0a6778784daa78e7
肖佳坤 <xiao@xiaojiakundeMacBook-Pro.local>
2024-05-12 23:26:16 +0800

    feat: showtest

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const loveApp = express();
+const app = express();
+const port = 3002;
+const lovePort = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+loveApp.get('/', (req, res) => {
+  res.send('❤️常莹!')
 })
+
+loveApp.listen(lovePort, () => {
+  console.log(`app listening on lovePort ${lovePort}`)
+});
+
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+})
+app.get('/test', (req, res) => {
+    res.json({a: 'test data'});
+    res.end()
+  })
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
-})
+    console.log(`app listening on lovePort ${lovePort}`)
+  })
